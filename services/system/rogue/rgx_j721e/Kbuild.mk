5d811cfb3eb3f54f0caf2f16f480050536b1da57
Gowtham Tammana <g-tammana@ti.com>
2020-08-11 11:11:31 -0500

    j721e: Add missing source in build file
    Signed-off-by: Gowtham Tammana <g-tammana@ti.com>

--- services/system/rogue/rgx_j721e/Kbuild.mk
+++ services/system/rogue/rgx_j721e/Kbuild.mk
@@ -42,6 +42,7 @@ PVRSRVKM_NAME = $(PVRSRV_MODNAME)
 
 $(PVRSRVKM_NAME)-y += services/system/$(PVR_ARCH)/$(PVR_SYSTEM)/sysconfig.o \
  services/system/$(PVR_ARCH)/common/env/linux/dma_support.o \
+ services/system/$(PVR_ARCH)/common/env/linux/interrupt_support.o \
  services/server/common/vmm_pvz_client.o \
  services/server/common/vmm_pvz_server.o \
  services/server/common/vz_vmm_pvz.o \
