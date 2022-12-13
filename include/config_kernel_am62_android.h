rcn-ee/ti-img-rogue-driver

commit 4a13b4d5facf2528a94a712ae35a410bb8207140

ddk: android: add support of Android Kernel Buils System[1] for AM62X
Google provide a kernel build system use to compile modules and kernel.
Link to Generic Kernel Image features we need to use wame env as AOSP.
DDK is not user friendly and it's not possible to use it without adding
specific files.

- add config_kernel.h generate at build time with DDK build sript and
rename it : config_kernel_am62_android.h
- add Makefile.

[1] : https://source.android.com/setup/build/building-kernels

Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

include/config_kernel_am62_android.h
@@ -1,0 +1,190 @@
+/*************************************************************************/ /*!
+@Copyright      Copyright (c) Imagination Technologies Ltd. All Rights Reserved
+@License        Dual MIT/GPLv2
+
+The contents of this file are subject to the MIT license as set out below.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+Alternatively, the contents of this file may be used under the terms of
+the GNU General Public License Version 2 ("GPL") in which case the provisions
+of GPL are applicable instead of those above.
+
+If you wish to allow use of your version of this file only under the terms of
+GPL, and not to allow others to use your version of this file under the terms
+of the MIT license, indicate your decision by deleting the provisions above
+and replace them with the notice and other provisions required by GPL as set
+out in the file called "GPL-COPYING" included in this distribution. If you do
+not delete the provisions above, a recipient may use your version of this file
+under the terms of either the MIT license or GPL.
+
+This License is also included in this distribution in the file called
+"MIT-COPYING".
+
+EXCEPT AS OTHERWISE STATED IN A NEGOTIATED AGREEMENT: (A) THE SOFTWARE IS
+PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE AND NONINFRINGEMENT; AND (B) IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/ /**************************************************************************/
+
+#define PVRSRV_APPHINT_FIRMWARE_HEAP_POLICY 5
+#define PVRSRV_ENABLE_CCCB_GROW
+#define RGX_FW_FILENAME "rgx.fw"
+#define RGX_SH_FILENAME "rgx.sh"
+#define PVR_BUILD_DIR "am62_android"
+#define PVR_BUILD_TYPE "release"
+#define PVRSRV_MODNAME "pvrsrvkm"
+#define PVRSYNC_MODNAME "pvr_sync"
+#define SUPPORT_RGX 1
+#define RELEASE
+#define SUPPORT_PHYSMEM_TEST
+#define PVRSRV_NEED_PVR_DPF
+#define PVRSRV_NEED_PVR_ASSERT
+#define SUPPORT_RGXTQ_BRIDGE
+#define PVRSRV_POISON_ON_ALLOC_VALUE 0xd9
+#define PVRSRV_POISON_ON_FREE_VALUE 0x63
+#define SUPPORT_MIPS_64K_PAGE_SIZE
+#define RGX_NUM_OS_SUPPORTED 1
+#define RGX_OSID_0_DEFAULT_PRIORITY (1 - 0)
+#define RGX_OSID_1_DEFAULT_PRIORITY (1 - 1)
+#define RGX_OSID_2_DEFAULT_PRIORITY (1 - 2)
+#define RGX_OSID_3_DEFAULT_PRIORITY (1 - 3)
+#define RGX_OSID_4_DEFAULT_PRIORITY (1 - 4)
+#define RGX_OSID_5_DEFAULT_PRIORITY (1 - 5)
+#define RGX_OSID_6_DEFAULT_PRIORITY (1 - 6)
+#define RGX_OSID_7_DEFAULT_PRIORITY (1 - 7)
+#define RGX_HCS_DEFAULT_DEADLINE_MS 0xFFFFFFFFU
+#define PVRSRV_APPHINT_DRIVERMODE 0x7FFFFFFF
+#define RGX_FW_HEAP_SHIFT 25
+#define SUPPORT_POWMON_COMPONENT
+#define PVR_POWER_ACTOR_MEASUREMENT_PERIOD_MS 10U
+#define PVR_POWER_MONITOR_HWPERF
+#define PVR_LDM_DRIVER_REGISTRATION_NAME "pvrsrvkm"
+#define PVRSRV_FULL_SYNC_TRACKING_HISTORY_LEN 256
+#define ION_DEFAULT_HEAP_NAME "reserved"
+#define ION_DEFAULT_HEAP_ID_MASK (1 << ION_HEAP_TYPE_SYSTEM)
+#define PVRSRV_APPHINT_HWRDEBUGDUMPLIMIT APPHNT_BLDVAR_DBGDUMPLIMIT
+#define PVRSRV_APPHINT_ENABLETRUSTEDDEVICEACECONFIG IMG_FALSE
+#define PVRSRV_APPHINT_GENERALNON4KHEAPPAGESIZE 0x4000
+#define PVRSRV_APPHINT_HWPERFCLIENTBUFFERSIZE 786432
+#define PVRSRV_APPHINT_ENABLESIGNATURECHECKS APPHNT_BLDVAR_ENABLESIGNATURECHECKS
+#define PVRSRV_APPHINT_SIGNATURECHECKSBUFSIZE RGXFW_SIG_BUFFER_SIZE_MIN
+#define PVRSRV_APPHINT_ENABLEFULLSYNCTRACKING IMG_FALSE
+#define PVRSRV_APPHINT_ENABLEPAGEFAULTDEBUG APPHNT_BLDVAR_ENABLEPAGEFAULTDEBUG
+#define PVRSRV_APPHINT_VALIDATEIRQ 0
+#define PVRSRV_APPHINT_DISABLECLOCKGATING 0
+#define PVRSRV_APPHINT_DISABLEDMOVERLAP 0
+#define PVRSRV_APPHINT_ENABLECDMKILLINGRANDMODE 0
+#define PVRSRV_APPHINT_ENABLERANDOMCONTEXTSWITCH 0
+#define PVRSRV_APPHINT_ENABLESOFTRESETCNTEXTSWITCH 0
+#define PVRSRV_APPHINT_ENABLEFWCONTEXTSWITCH RGXFWIF_INICFG_OS_CTXSWITCH_DM_ALL
+#define PVRSRV_APPHINT_VDMCONTEXTSWITCHMODE RGXFWIF_INICFG_VDM_CTX_STORE_MODE_INDEX
+#define PVRSRV_APPHINT_ENABLERDPOWERISLAND RGX_RD_POWER_ISLAND_DEFAULT
+#define PVRSRV_APPHINT_FIRMWAREPERF FW_PERF_CONF_NONE
+#define PVRSRV_APPHINT_FWCONTEXTSWITCHPROFILE RGXFWIF_CTXSWITCH_PROFILE_MEDIUM_EN
+#define PVRSRV_APPHINT_HWPERFDISABLECUSTOMCOUNTERFILTER 0
+#define PVRSRV_APPHINT_HWPERFFWBUFSIZEINKB 2048
+#define PVRSRV_APPHINT_HWPERFHOSTBUFSIZEINKB 2048
+#define PVRSRV_APPHINT_HWPERFHOSTTHREADTIMEOUTINMS 50
+#define PVRSRV_APPHINT_JONESDISABLEMASK 0
+#define PVRSRV_APPHINT_NEWFILTERINGMODE 1
+#define PVRSRV_APPHINT_TRUNCATEMODE 0
+#define PVRSRV_APPHINT_EMUMAXFREQ 0
+#define PVRSRV_APPHINT_GPIOVALIDATIONMODE 0
+#define PVRSRV_APPHINT_RGXBVNC ""
+#define PVRSRV_APPHINT_CLEANUPTHREADPRIORITY 5
+#define PVRSRV_APPHINT_WATCHDOGTHREADPRIORITY 0
+#define PVRSRV_APPHINT_CACHEOPTHREADPRIORITY 1
+#define PVRSRV_APPHINT_ASSERTONHWRTRIGGER IMG_FALSE
+#define PVRSRV_APPHINT_ASSERTOUTOFMEMORY IMG_FALSE
+#define PVRSRV_APPHINT_CHECKMLIST APPHNT_BLDVAR_DEBUG
+#define PVRSRV_APPHINT_DISABLEFEDLOGGING IMG_FALSE
+#define PVRSRV_APPHINT_KCCB_SIZE_LOG2 7
+#define PVRSRV_APPHINT_ENABLEAPM RGX_ACTIVEPM_DEFAULT
+#define PVRSRV_APPHINT_ENABLEHTBLOGGROUP 0
+#define PVRSRV_APPHINT_ENABLELOGGROUP RGXFWIF_LOG_TYPE_NONE
+#define PVRSRV_APPHINT_FIRMWARELOGTYPE 0
+#define PVRSRV_APPHINT_FWTRACEBUFSIZEINDWORDS RGXFW_TRACE_BUF_DEFAULT_SIZE_IN_DWORDS
+#define PVRSRV_APPHINT_FBCDCVERSIONOVERRIDE 0
+#define PVRSRV_APPHINT_HTBOPERATIONMODE HTB_OPMODE_DROPOLDEST
+#define PVRSRV_APPHINT_HTBUFFERSIZE 64
+#define PVRSRV_APPHINT_ENABLEFTRACEGPU IMG_FALSE
+#define PVRSRV_APPHINT_HWPERFFWFILTER 0
+#define PVRSRV_APPHINT_HWPERFHOSTFILTER 0
+#define PVRSRV_APPHINT_HWPERFCLIENTFILTER_SERVICES 0
+#define PVRSRV_APPHINT_HWPERFCLIENTFILTER_EGL 0
+#define PVRSRV_APPHINT_HWPERFCLIENTFILTER_OPENGLES 0
+#define PVRSRV_APPHINT_HWPERFCLIENTFILTER_OPENCL 0
+#define PVRSRV_APPHINT_HWPERFCLIENTFILTER_VULKAN 0
+#define PVRSRV_APPHINT_HWPERFCLIENTFILTER_OPENGL 0
+#define PVRSRV_APPHINT_TIMECORRCLOCK 0
+#define PVRSRV_APPHINT_ENABLEFWPOISONONFREE IMG_FALSE
+#define PVRSRV_APPHINT_FWPOISONONFREEVALUE 0xBD
+#define PVRSRV_APPHINT_ZEROFREELIST IMG_FALSE
+#define PVRSRV_APPHINT_GPUUNITSPOWERCHANGE IMG_FALSE
+#define PVRSRV_APPHINT_DISABLEPDUMPPANIC IMG_FALSE
+#define PVRSRV_APPHINT_CACHEOPCONFIG 0
+#define PVRSRV_APPHINT_CACHEOPUMKMHRESHOLDSIZE 0
+#define PVRSRV_APPHINT_IGNOREHWREPORTEDBVNC IMG_FALSE
+#define PVRSRV_APPHINT_PHYSMEMTESTPASSES APPHNT_PHYSMEMTEST_ENABLE
+#define PVRSRV_APPHINT_TESTSLRINTERVAL 0
+#define PVRSRV_APPHINT_RISCVDMITEST 0
+#define SOC_TIMER_FREQ 20
+#define PDVFS_COM_HOST 1
+#define PDVFS_COM_AP 2
+#define PDVFS_COM_PMC 3
+#define PDVFS_COM_IMG_CLKDIV 4
+#define PDVFS_COM PDVFS_COM_HOST
+#define PVR_GPIO_MODE_GENERAL 1
+#define PVR_GPIO_MODE_POWMON_PIN 2
+#define PVR_GPIO_MODE PVR_GPIO_MODE_GENERAL
+#define PVRSRV_ENABLE_PROCESS_STATS
+#define SUPPORT_USC_BREAKPOINT
+#define RGXFW_SAFETY_WATCHDOG_PERIOD_IN_US 1000000
+#define PVR_ANNOTATION_MAX_LEN 63
+#define SUPPORT_DI_BRG_IMPL
+#define PVRSRV_ENABLE_MEMTRACK_STATS_FILE
+#define PVR_LINUX_PHYSMEM_MAX_POOL_PAGES 10240
+#define PVR_LINUX_PHYSMEM_MAX_EXCESS_POOL_PAGES 20480
+#define PVR_LINUX_PHYSMEM_ZERO_ALL_PAGES
+#define PVR_DIRTY_BYTES_FLUSH_THRESHOLD 524288
+#define PVR_LINUX_HIGHORDER_ALLOCATION_THRESHOLD 256
+#define PVR_LINUX_PHYSMEM_MAX_ALLOC_ORDER_NUM 2
+#define PVR_LINUX_KMALLOC_ALLOCATION_THRESHOLD 16384
+#define SUPPORT_NATIVE_FENCE_SYNC
+#define PVRSRV_STALLED_CCB_ACTION
+#define UPDATE_FENCE_CHECKPOINT_COUNT 1
+#define PVR_DRM_NAME "pvr"
+#define DEVICE_MEMSETCPY_ALIGN_IN_BYTES 16
+#define RGX_INITIAL_SLR_HOLDOFF_PERIOD_MS 0
+#define PVRSRV_RGX_LOG2_CLIENT_CCB_SIZE_TQ3D 14
+#define PVRSRV_RGX_LOG2_CLIENT_CCB_SIZE_TQ2D 14
+#define PVRSRV_RGX_LOG2_CLIENT_CCB_SIZE_CDM 13
+#define PVRSRV_RGX_LOG2_CLIENT_CCB_SIZE_TA 15
+#define PVRSRV_RGX_LOG2_CLIENT_CCB_SIZE_3D 16
+#define PVRSRV_RGX_LOG2_CLIENT_CCB_SIZE_KICKSYNC 13
+#define PVRSRV_RGX_LOG2_CLIENT_CCB_SIZE_TDM 14
+#define PVRSRV_RGX_LOG2_CLIENT_CCB_MAX_SIZE_TQ3D 17
+#define PVRSRV_RGX_LOG2_CLIENT_CCB_MAX_SIZE_TQ2D 17
+#define PVRSRV_RGX_LOG2_CLIENT_CCB_MAX_SIZE_CDM 15
+#define PVRSRV_RGX_LOG2_CLIENT_CCB_MAX_SIZE_TA 16
+#define PVRSRV_RGX_LOG2_CLIENT_CCB_MAX_SIZE_3D 17
+#define PVRSRV_RGX_LOG2_CLIENT_CCB_MAX_SIZE_KICKSYNC 13
+#define PVRSRV_RGX_LOG2_CLIENT_CCB_MAX_SIZE_TDM 17
+#define ANDROID
+#define DMABUF_DEFAULT_HEAP_NAME "system"
+#define PVR_ANDROID_ION_HEADER "linux/ion.h"
+#define PVR_ANDROID_ION_PRIV_HEADER "../drivers/gpu/ion/ion_priv.h"
+#define PVR_ANDROID_SYNC_HEADER "linux/sync.h"
+#define PVRSRV_ENABLE_PVR_ION_STATS

Makefile
@@ -1,0 +1,271 @@
+########################################################################### ###
+#@Copyright     Copyright (c) Imagination Technologies Ltd. All Rights Reserved
+#@License       Dual MIT/GPLv2
+#
+# The contents of this file are subject to the MIT license as set out below.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# Alternatively, the contents of this file may be used under the terms of
+# the GNU General Public License Version 2 ("GPL") in which case the provisions
+# of GPL are applicable instead of those above.
+#
+# If you wish to allow use of your version of this file only under the terms of
+# GPL, and not to allow others to use your version of this file under the terms
+# of the MIT license, indicate your decision by deleting the provisions above
+# and replace them with the notice and other provisions required by GPL as set
+# out in the file called "GPL-COPYING" included in this distribution. If you do
+# not delete the provisions above, a recipient may use your version of this file
+# under the terms of either the MIT license or GPL.
+#
+# This License is also included in this distribution in the file called
+# "MIT-COPYING".
+#
+# EXCEPT AS OTHERWISE STATED IN A NEGOTIATED AGREEMENT: (A) THE SOFTWARE IS
+# PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+# BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+# PURPOSE AND NONINFRINGEMENT; AND (B) IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+### ###########################################################################
+
+obj-m += pvrsrvkm.o
+
+
+pvrsrvkm-y += \
+	generated/rogue/cache_bridge/server_cache_bridge.o \
+	generated/rogue/cache_bridge/client_cache_direct_bridge.o \
+	generated/rogue/cmm_bridge/server_cmm_bridge.o \
+	generated/rogue/devicememhistory_bridge/server_devicememhistory_bridge.o \
+	generated/rogue/devicememhistory_bridge/client_devicememhistory_direct_bridge.o \
+	generated/rogue/di_bridge/server_di_bridge.o \
+	generated/rogue/dmabuf_bridge/server_dmabuf_bridge.o \
+	generated/rogue/htbuffer_bridge/client_htbuffer_direct_bridge.o \
+	generated/rogue/htbuffer_bridge/server_htbuffer_bridge.o \
+	generated/rogue/mm_bridge/client_mm_direct_bridge.o \
+	generated/rogue/mm_bridge/server_mm_bridge.o \
+	generated/rogue/pvrtl_bridge/client_pvrtl_direct_bridge.o \
+	generated/rogue/pvrtl_bridge/server_pvrtl_bridge.o \
+	generated/rogue/rgxbreakpoint_bridge/server_rgxbreakpoint_bridge.o \
+	generated/rogue/rgxcmp_bridge/server_rgxcmp_bridge.o \
+	generated/rogue/rgxfwdbg_bridge/server_rgxfwdbg_bridge.o \
+	generated/rogue/rgxhwperf_bridge/server_rgxhwperf_bridge.o \
+	generated/rogue/rgxkicksync_bridge/server_rgxkicksync_bridge.o \
+	generated/rogue/rgxregconfig_bridge/server_rgxregconfig_bridge.o \
+	generated/rogue/rgxta3d_bridge/server_rgxta3d_bridge.o \
+	generated/rogue/rgxtimerquery_bridge/server_rgxtimerquery_bridge.o \
+	generated/rogue/rgxtq2_bridge/server_rgxtq2_bridge.o \
+	generated/rogue/rgxtq_bridge/server_rgxtq_bridge.o \
+	generated/rogue/srvcore_bridge/server_srvcore_bridge.o \
+	generated/rogue/sync_bridge/client_sync_direct_bridge.o \
+	generated/rogue/sync_bridge/server_sync_bridge.o \
+	generated/rogue/synctracking_bridge/client_synctracking_direct_bridge.o \
+	generated/rogue/synctracking_bridge/server_synctracking_bridge.o \
+	services/server/common/cache_km.o \
+	services/server/common/connection_server.o \
+	services/server/common/debug_common.o \
+	services/server/common/devicemem_heapcfg.o \
+	services/server/common/devicemem_history_server.o \
+	services/server/common/devicemem_server.o \
+	services/server/common/di_impl_brg.o \
+	services/server/common/di_server.o \
+	services/server/common/handle.o \
+	services/server/common/htb_debug.o \
+	services/server/common/htbserver.o \
+	services/server/common/info_page_km.o \
+	services/server/common/lists.o \
+	services/server/common/mmu_common.o \
+	services/server/common/physheap.o \
+	services/server/common/physmem.o \
+	services/server/common/physmem_hostmem.o \
+	services/server/common/physmem_lma.o \
+	services/server/common/pmr.o \
+	services/server/common/power.o \
+	services/server/common/process_stats.o \
+	services/server/common/pvr_notifier.o \
+	services/server/common/pvrsrv.o \
+	services/server/common/pvrsrv_bridge_init.o \
+	services/server/common/pvrsrv_pool.o \
+	services/server/common/srvcore.o \
+	services/server/common/sync_checkpoint.o \
+	services/server/common/sync_server.o \
+	services/server/common/tlintern.o \
+	services/server/common/tlserver.o \
+	services/server/common/tlstream.o \
+	services/server/common/vmm_pvz_client.o \
+	services/server/common/vmm_pvz_server.o \
+	services/server/common/vz_vmm_pvz.o \
+	services/server/common/vz_vmm_vm.o \
+	services/server/devices/rgx_bridge_init.o \
+	services/server/devices/rgxbreakpoint.o \
+	services/server/devices/rgxbvnc.o \
+	services/server/devices/rgxccb.o \
+	services/server/devices/rgxcompute.o \
+	services/server/devices/rgxfwdbg.o \
+	services/server/devices/rgxfwimageutils.o \
+	services/server/devices/rgxfwtrace_strings.o \
+	services/server/devices/rgxhwperf_common.o \
+	services/server/devices/rgxkicksync.o \
+	services/server/devices/rgxmem.o \
+	services/server/devices/rgxregconfig.o \
+	services/server/devices/rgxshader.o \
+	services/server/devices/rgxsyncutils.o \
+	services/server/devices/rgxtdmtransfer.o \
+	services/server/devices/rgxtimecorr.o \
+	services/server/devices/rgxtimerquery.o \
+	services/server/devices/rgxutils.o \
+	services/server/devices/rogue/rgxdebug.o \
+	services/server/devices/rogue/rgxfwutils.o \
+	services/server/devices/rogue/rgxhwperf.o \
+	services/server/devices/rogue/rgxinit.o \
+	services/server/devices/rogue/rgxlayer_impl.o \
+	services/server/devices/rogue/rgxmipsmmuinit.o \
+	services/server/devices/rogue/rgxmmuinit.o \
+	services/server/devices/rogue/rgxmulticore.o \
+	services/server/devices/rogue/rgxpower.o \
+	services/server/devices/rogue/rgxsrvinit.o \
+	services/server/devices/rogue/rgxstartstop.o \
+	services/server/devices/rogue/rgxta3d.o \
+	services/server/devices/rogue/rgxtransfer.o \
+	services/server/env/linux/allocmem.o \
+	services/server/env/linux/event.o \
+	services/server/env/linux/fwload.o \
+	services/server/env/linux/handle_idr.o \
+	services/server/env/linux/km_apphint.o \
+	services/server/env/linux/module_common.o \
+	services/server/env/linux/osconnection_server.o \
+	services/server/env/linux/osfunc.o \
+	services/server/env/linux/osmmap_stub.o \
+	services/server/env/linux/physmem_dmabuf.o \
+	services/server/env/linux/physmem_osmem_linux.o \
+	services/server/env/linux/physmem_test.o \
+	services/server/env/linux/pmr_os.o \
+	services/server/env/linux/pvr_bridge_k.o \
+	services/server/env/linux/pvr_counting_timeline.o \
+	services/server/env/linux/pvr_debug.o \
+	services/server/env/linux/pvr_drm.o \
+	services/server/env/linux/pvr_fence.o \
+	services/server/env/linux/pvr_gputrace.o \
+	services/server/env/linux/pvr_platform_drv.o \
+	services/server/env/linux/pvr_procfs.o \
+	services/server/env/linux/pvr_sw_fence.o \
+	services/server/env/linux/pvr_sync_file.o \
+	services/server/env/linux/pvr_sync_ioctl_common.o \
+	services/server/env/linux/pvr_sync_ioctl_dev.o \
+	services/shared/common/devicemem.o \
+	services/shared/common/devicemem_utils.o \
+	services/shared/common/hash.o \
+	services/shared/common/htbuffer.o \
+	services/shared/common/mem_utils.o \
+	services/shared/common/pvrsrv_error.o \
+	services/shared/common/ra.o \
+	services/shared/common/sync.o \
+	services/shared/common/tlclient.o \
+	services/shared/common/uniq_key_splay_tree.o \
+	services/shared/devices/rogue/rgx_hwperf_table.o \
+	services/system/common/env/linux/interrupt_support.o \
+	services/system/common/sysconfig_cmn.o \
+	services/system/rogue/common/env/linux/dma_support.o \
+	services/system/rogue/common/vmm_type_stub.o
+
+pvrsrvkm-y += \
+	services/system/rogue/axe_am62/sysconfig.o
+
+pvrsrvkm-$(CONFIG_ARM)   += services/server/env/linux/osfunc_arm.o
+pvrsrvkm-$(CONFIG_ARM64) += services/server/env/linux/osfunc_arm64.o
+pvrsrvkm-$(CONFIG_EVENT_TRACING) += services/server/env/linux/trace_events.o
+pvrsrvkm-$(CONFIG_X86)   += services/server/env/linux/osfunc_x86.o
+
+
+ccflags-y += \
+ -include $(srctree)/../imgtech-module/include/config_kernel_am62_android.h \
+ -I $(srctree)/../imgtech-module/ \
+ -I $(srctree)/../imgtech-module/generated/rogue/rgxcmp_bridge \
+ -I $(srctree)/../imgtech-module/generated/rogue/cache_bridge \
+ -I $(srctree)/../imgtech-module/generated/rogue/cmm_bridge \
+ -I $(srctree)/../imgtech-module/generated/rogue/devicememhistory_bridge \
+ -I $(srctree)/../imgtech-module/generated/rogue/di_bridge \
+ -I $(srctree)/../imgtech-module/generated/rogue/mm_bridge \
+ -I $(srctree)/../imgtech-module/generated/rogue/dmabuf_bridge \
+ -I $(srctree)/../imgtech-module/generated/rogue/htbuffer_bridge \
+ -I $(srctree)/../imgtech-module/generated/rogue/pvrtl_bridge \
+ -I $(srctree)/../imgtech-module/generated/rogue/rgxbreakpoint_bridge \
+ -I $(srctree)/../imgtech-module/generated/rogue/rgxfwdbg_bridge \
+ -I $(srctree)/../imgtech-module/generated/rogue/rgxhwperf_bridge \
+ -I $(srctree)/../imgtech-module/generated/rogue/rgxkicksync_bridge \
+ -I $(srctree)/../imgtech-module/generated/rogue/rgxregconfig_bridge \
+ -I $(srctree)/../imgtech-module/generated/rogue/rgxta3d_bridge \
+ -I $(srctree)/../imgtech-module/generated/rogue/rgxtq2_bridge \
+ -I $(srctree)/../imgtech-module/generated/rogue/rgxtq_bridge \
+ -I $(srctree)/../imgtech-module/generated/rogue/rgxsignals_bridge \
+ -I $(srctree)/../imgtech-module/generated/rogue/rgxtimerquery_bridge \
+ -I $(srctree)/../imgtech-module/generated/rogue/srvcore_bridge \
+ -I $(srctree)/../imgtech-module/generated/rogue/sync_bridge \
+ -I $(srctree)/../imgtech-module/generated/rogue/synctracking_bridge \
+ -I $(srctree)/../imgtech-module/kernel/drivers/staging/imgtec \
+ -I $(srctree)/../imgtech-module/include \
+ -I $(srctree)/../imgtech-module/include/drm \
+ -I $(srctree)/../imgtech-module/include/rogue \
+ -I $(srctree)/../imgtech-module/include/public \
+ -I $(srctree)/../imgtech-module/include/system \
+ -I $(srctree)/../imgtech-module/services/include \
+ -I $(srctree)/../imgtech-module/services/include/env/linux \
+ -I $(srctree)/../imgtech-module/services/include/rogue \
+ -I $(srctree)/../imgtech-module/services/server/include \
+ -I $(srctree)/../imgtech-module/services/server/env/linux/ \
+ -I $(srctree)/../imgtech-module/services/server/common/ \
+ -I $(srctree)/../imgtech-module/services/server/devices/ \
+ -I $(srctree)/../imgtech-module/services/server/devices/rogue/ \
+ -I $(srctree)/../imgtech-module/services/shared/common \
+ -I $(srctree)/../imgtech-module/services/shared/devices/rogue \
+ -I $(srctree)/../imgtech-module/services/shared/include \
+ -I $(srctree)/../imgtech-module/services/system/include \
+ -I $(srctree)/../imgtech-module/services/system/rogue/axe_am62 \
+ -I $(srctree)/../imgtech-module/services/system/rogue/include \
+ -I $(srctree)/../imgtech-module/hwdefs/rogue/km \
+ -I $(srctree)/../imgtech-module/hwdefs/rogue \
+ -I $(srctree)/include/uapi/linux \
+ -I $(srctree)/include/linux \
+ -I $(srctree)/include/drm \
+ -I $(srctree)/include/ \
+ -I $(srctree)/arch/arm64/include/asm/ \
+ -I $(srctree)/arch/arm/include/asm/ \
+ -D__linux__
+
+ccflags-y += \
+	-Wno-missing-field-initializers \
+	-Wdeclaration-after-statement \
+	-Wno-format-zero-length \
+	-Wmissing-prototypes \
+	-Wstrict-prototypes \
+	-Wno-unused-parameter \
+	-Wno-sign-compare \
+	-Wno-type-limits \
+	-Wno-error \
+	-Wno-typedef-redefinition
+
+
+KERNEL_SRC ?= /lib/modules/$(shell uname -r)/build
+
+
+ # we get the symbols from modules using KBUILD_EXTRA_SYMBOLS to prevent warnings about unknown functions
+all:
+	$(Q)$(MAKE) -C $(KERNEL_SRC)  M=$(M)
+
+modules_install:
+	$(Q)$(MAKE) -C $(KERNEL_SRC) M=$(M) modules_install
+
+clean:
+	$(Q)$(MAKE) -C $(KERNEL_SRC) M=$(M) clean
+
+FORCE:
+.PHONY: FORCE

commit 36f60f0e673ee8c53cd782cf6f788e42186cdf06

ddk: android: fix build from android kernel build system
config_kernel_am62_android.h file was not updated in previous commit so
actually driver not building in Android kernel build system.

Align this file with config_kernel.h file generate by DDK build system.

Fixes: 4a13b4d5facf ("ddk: android: add support of Android Kernel Buils
System[1] for AM62X")
Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

include/config_kernel_am62_android.h
@@ -47,14 +47,16 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #define PVRSRV_MODNAME "pvrsrvkm"
 #define PVRSYNC_MODNAME "pvr_sync"
 #define SUPPORT_RGX 1
+#define USE_PVRSYNC_DEVNODE
+#define PVRSRV_HWPERF_COUNTERS_PERBLK 12
 #define RELEASE
-#define SUPPORT_PHYSMEM_TEST
+#define RGX_BVNC_CORE_KM_HEADER "cores/rgxcore_km_33.15.11.3.h"
+#define RGX_BNC_CONFIG_KM_HEADER "configs/rgxconfig_km_33.V.11.3.h"
 #define PVRSRV_NEED_PVR_DPF
-#define PVRSRV_NEED_PVR_ASSERT
+#define SUPPORT_PHYSMEM_TEST
 #define SUPPORT_RGXTQ_BRIDGE
 #define PVRSRV_POISON_ON_ALLOC_VALUE 0xd9
 #define PVRSRV_POISON_ON_FREE_VALUE 0x63
-#define SUPPORT_MIPS_64K_PAGE_SIZE
 #define RGX_NUM_OS_SUPPORTED 1
 #define RGX_OSID_0_DEFAULT_PRIORITY (1 - 0)
 #define RGX_OSID_1_DEFAULT_PRIORITY (1 - 1)
@@ -70,6 +72,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #define SUPPORT_POWMON_COMPONENT
 #define PVR_POWER_ACTOR_MEASUREMENT_PERIOD_MS 10U
 #define PVR_POWER_MONITOR_HWPERF
+#define PVR_LDM_PLATFORM_PRE_REGISTERED
 #define PVR_LDM_DRIVER_REGISTRATION_NAME "pvrsrvkm"
 #define PVRSRV_FULL_SYNC_TRACKING_HISTORY_LEN 256
 #define ION_DEFAULT_HEAP_NAME "reserved"
@@ -89,14 +92,16 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #define PVRSRV_APPHINT_ENABLERANDOMCONTEXTSWITCH 0
 #define PVRSRV_APPHINT_ENABLESOFTRESETCNTEXTSWITCH 0
 #define PVRSRV_APPHINT_ENABLEFWCONTEXTSWITCH RGXFWIF_INICFG_OS_CTXSWITCH_DM_ALL
-#define PVRSRV_APPHINT_VDMCONTEXTSWITCHMODE RGXFWIF_INICFG_VDM_CTX_STORE_MODE_INDEX
 #define PVRSRV_APPHINT_ENABLERDPOWERISLAND RGX_RD_POWER_ISLAND_DEFAULT
+#define PVRSRV_APPHINT_ENABLESPUCLOCKGATING IMG_FALSE
 #define PVRSRV_APPHINT_FIRMWAREPERF FW_PERF_CONF_NONE
 #define PVRSRV_APPHINT_FWCONTEXTSWITCHPROFILE RGXFWIF_CTXSWITCH_PROFILE_MEDIUM_EN
 #define PVRSRV_APPHINT_HWPERFDISABLECUSTOMCOUNTERFILTER 0
 #define PVRSRV_APPHINT_HWPERFFWBUFSIZEINKB 2048
 #define PVRSRV_APPHINT_HWPERFHOSTBUFSIZEINKB 2048
 #define PVRSRV_APPHINT_HWPERFHOSTTHREADTIMEOUTINMS 50
+#define PVRSRV_APPHINT_TFBCCOMPRESSIONCONTROLGROUP 1
+#define PVRSRV_APPHINT_TFBCCOMPRESSIONCONTROLSCHEME 0
 #define PVRSRV_APPHINT_JONESDISABLEMASK 0
 #define PVRSRV_APPHINT_NEWFILTERINGMODE 1
 #define PVRSRV_APPHINT_TRUNCATEMODE 0
@@ -140,6 +145,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #define PVRSRV_APPHINT_PHYSMEMTESTPASSES APPHNT_PHYSMEMTEST_ENABLE
 #define PVRSRV_APPHINT_TESTSLRINTERVAL 0
 #define PVRSRV_APPHINT_RISCVDMITEST 0
+#define PVRSRV_APPHINT_VALIDATESOCUSCTIMERS 0
 #define SOC_TIMER_FREQ 20
 #define PDVFS_COM_HOST 1
 #define PDVFS_COM_AP 2
@@ -153,6 +159,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #define SUPPORT_USC_BREAKPOINT
 #define RGXFW_SAFETY_WATCHDOG_PERIOD_IN_US 1000000
 #define PVR_ANNOTATION_MAX_LEN 63
+#define PVRSRV_DEVICE_INIT_MODE PVRSRV_LINUX_DEV_INIT_ON_OPEN
 #define SUPPORT_DI_BRG_IMPL
 #define PVRSRV_ENABLE_MEMTRACK_STATS_FILE
 #define PVR_LINUX_PHYSMEM_MAX_POOL_PAGES 10240
@@ -162,6 +169,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #define PVR_LINUX_HIGHORDER_ALLOCATION_THRESHOLD 256
 #define PVR_LINUX_PHYSMEM_MAX_ALLOC_ORDER_NUM 2
 #define PVR_LINUX_KMALLOC_ALLOCATION_THRESHOLD 16384
+#define PVRSRV_USE_LINUX_CONFIG_INIT_ON_ALLOC 1
 #define SUPPORT_NATIVE_FENCE_SYNC
 #define PVRSRV_STALLED_CCB_ACTION
 #define UPDATE_FENCE_CHECKPOINT_COUNT 1
@@ -175,6 +183,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #define PVRSRV_RGX_LOG2_CLIENT_CCB_SIZE_3D 16
 #define PVRSRV_RGX_LOG2_CLIENT_CCB_SIZE_KICKSYNC 13
 #define PVRSRV_RGX_LOG2_CLIENT_CCB_SIZE_TDM 14
+#define PVRSRV_RGX_LOG2_CLIENT_CCB_SIZE_RDM 13
 #define PVRSRV_RGX_LOG2_CLIENT_CCB_MAX_SIZE_TQ3D 17
 #define PVRSRV_RGX_LOG2_CLIENT_CCB_MAX_SIZE_TQ2D 17
 #define PVRSRV_RGX_LOG2_CLIENT_CCB_MAX_SIZE_CDM 15
@@ -182,9 +191,11 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #define PVRSRV_RGX_LOG2_CLIENT_CCB_MAX_SIZE_3D 17
 #define PVRSRV_RGX_LOG2_CLIENT_CCB_MAX_SIZE_KICKSYNC 13
 #define PVRSRV_RGX_LOG2_CLIENT_CCB_MAX_SIZE_TDM 17
+#define PVRSRV_RGX_LOG2_CLIENT_CCB_MAX_SIZE_RDM 15
 #define ANDROID
+#define SUPPORT_DMA_HEAP
 #define DMABUF_DEFAULT_HEAP_NAME "system"
 #define PVR_ANDROID_ION_HEADER "linux/ion.h"
 #define PVR_ANDROID_ION_PRIV_HEADER "../drivers/gpu/ion/ion_priv.h"
 #define PVR_ANDROID_SYNC_HEADER "linux/sync.h"
-#define PVRSRV_ENABLE_PVR_ION_STATS
+#define PVR_ANDROID_HAS_DMA_HEAP_FIND