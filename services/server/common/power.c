3bcbba9708fe6a882cd0f4820469d026fff7aee5
Randolph Sapp <rs@ti.com>
2025-03-14 14:52:54 -0500

    power: ignore requests for uninitialized devices
    Because this driver uses lazy initialization, if the GPU is never given
    a task it will not be initialized. Even if the device is not fully
    initialized it is still registered with the DRM subsystem and capable of
    receiving OS APM signals.
    
    As such these events should not result in a fatal error by default. They
    should, however, still be logged in debug builds. Outside of the
    behavior described above, it is normally the result of a fault to make
    it to this section of code with an uninitialized device.
    
    Signed-off-by: Randolph Sapp <rs@ti.com>

--- services/server/common/power.c
+++ services/server/common/power.c
@@ -1059,10 +1059,10 @@ PVRSRVSetDeviceSystemPowerState(PPVRSRV_DEVICE_NODE psDeviceNode,
 	}
 
 	if (psDeviceNode->eDevState < PVRSRV_DEVICE_STATE_ACTIVE) {
-		/* Power device is not initialised. */
-		PVR_DPF((PVR_DBG_ERROR, "%s: Device not initialised",
+		/* Power device is not initialised. Ignoring request. */
+		PVR_DPF((PVR_DBG_MESSAGE, "%s: Device not initialised, ignoring request",
 			 __func__));
-		return PVRSRV_ERROR_INVALID_PARAMS;
+		return PVRSRV_OK;
 	}
 
 	/* Prevent simultaneous SetPowerStateKM calls */
